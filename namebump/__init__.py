f82f7103cba348acf8655f9ef63fbcb5f68709f6
Matthew Roberts <matthew@roberts.pm>
2026-02-05 18:14:04 +1100

    Install.

--- namebump/__init__.py
+++ namebump/__init__.py
@@ -2,6 +2,6 @@ import sys
 
 # Prevent double imports.
 if not '-m' in sys.argv:
-    from .client import Client, get, put, delete, DO_BUMP, DONT_BUMP, THROW_BUMP
+    from .defs import DO_BUMP, DONT_BUMP, THROW_BUMP
+    from .client import Client, get, put, delete
     from .keypair import Keypair
-    from .defs import *
