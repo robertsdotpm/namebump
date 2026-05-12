2c64e318504142477c4242ca13a95dd6257f81f2
Matthew Roberts <matthew@roberts.pm>
2026-05-12 22:37:41 +1000

    bump version.

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="namebump",
-    version="0.0.10",
+    version="0.0.11",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=["aionetiface", "ecdsa"],
+    python_requires=">=3.5",
 )
